cf47b4327231a9cef72ba977d0ad67f887e31829
Qt君 <2088201923@qq.com>
2018-03-07 15:12:57 +0800

    增加创建表格与插入到指定表格的api

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
+#include "SqlService.h"
 #include <QCoreApplication>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    SqlService sql;
+    sql.open("aa.db");
+    QMap<QString, QString> map;
+    map["id"] = "int primary key";
+    map["type"] = "varchar";
+    sql.createTable("bb", map);
+    QMap<QString, QVariant> mapContent;
+    mapContent["id"] = 2;
+    mapContent["type"] = "high";
+    sql.insertTable("bb", mapContent);
 
-    qDebug()<<"start sql test";
+    qDebug()<<sql.lastError();
     return a.exec();
 }
