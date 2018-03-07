d89f9d8c0762c6acf1ce0a79b90bcba63c6a3644
strong <chenfaqiang@ema-tech.com>
2018-03-07 15:12:57 +0800

    增加创建表格与插入到指定表格的api

--- SqlService.cpp
+++ SqlService.cpp
@@ -1,4 +1,6 @@
 #include "SqlService.h"
+#include <QSqlError>
+#include <QDebug>
 
 SqlService::SqlService()
 {
@@ -12,24 +14,141 @@ bool SqlService::open(QString name)
         m_sqlDatabase = QSqlDatabase::database(name);
     }
     else {
-        m_sqlDatabase = QSqlDatabase::addDatabase("QSQLITE", name);//添加数据库驱动
-        m_sqlDatabase.setDatabaseName(name); //数据库连接命名
+        /* 添加数据库驱动 */
+        m_sqlDatabase = QSqlDatabase::addDatabase("QSQLITE", name);
+        /* 数据库连接命名 */
+        m_sqlDatabase.setDatabaseName(name);
     }
 
-    if(!m_sqlDatabase.open()) { //打开数据库
-         qDebug()<<"open failed!"<<m_sqlDatabase.lastError();
+    /* 打开数据库 */
+    if(!m_sqlDatabase.open()) {
+         m_lastError = m_sqlDatabase.lastError().text();
          return false;
     }
 
-    m_sqlQuery = QSqlQuery(m_sqlDatabase); //以下执行相关QSL语句
+    /* 以下执行相关QSL语句 */
+    m_sqlQuery = QSqlQuery(m_sqlDatabase);
+    return true;
+}
+
+bool SqlService::createTable(QString table, QMap<QString, QString> map)
+{
+    QString tableList = QString("create table %1 (").arg(table);
+    QMapIterator<QString, QString> i(map);
+    while (i.hasNext()) {
+        i.next();
+        tableList.append(QString("%1 %2").arg(i.key()).arg(i.value()));
+        if (i.hasNext())
+            tableList.append(", ");
+    }
+    tableList.append(")");
+
+    if(!isTableExist(table)) {
+        if (!m_sqlQuery.exec(QString(tableList))) {
+            m_lastError = m_sqlQuery.lastError().text();
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+    else {
+        m_lastError = QString("Exist table<%1>!").arg(table);
+        return false;
+    }
+}
+
+bool SqlService::insertTable(QString table, QMap<QString, QVariant> map)
+{
+
+    QMap<QString, QString> tableContentMap;
+    if (!isTableExist(table)) {
+        m_lastError = QString("Not find %1 table!").arg(table);
+        return false;
+    }
+    else {
+        tableContentMap = getTableInfo(table);
+        if (tableContentMap.isEmpty())
+            return false;
+    }
+
+    QString insertTableContent = QString("insert into %1 (").arg(table);
+    QString values = QString("values (");
+
+    QMapIterator<QString, QString> i(tableContentMap);
+    while (i.hasNext()) {
+        i.next();
+        insertTableContent.append(QString("%1").arg(i.key()));
+        values.append("?");
+        if (i.hasNext()) {
+            insertTableContent.append(", ");
+            values.append(", ");
+        }
+    }
+    insertTableContent.append(") ");
+    values.append(")");
+
+    insertTableContent.append(values);
+
+    if (!m_sqlQuery.prepare(insertTableContent)) {
+        m_lastError = m_sqlQuery.lastError().text();
+        return false;
+    }
+
+    QMapIterator<QString, QString> ii(tableContentMap);
+    while (ii.hasNext()) {
+        ii.next();
+        m_sqlQuery.addBindValue(map[ii.key()]);
+    }
+
+    if (!m_sqlQuery.exec()) {
+        m_lastError = m_sqlQuery.lastError().text();
+        return false;
+    }
+    return true;
+}
+
+QString SqlService::lastError()
+{
+    return m_lastError;
 }
 
 QSqlQuery &SqlService::getSqlQuery()
 {
-    return &m_sqlQuery;
+    return m_sqlQuery;
 }
 
 QSqlDatabase &SqlService::getSqlDatabase()
 {
-    return &m_sqlDatabase;
+    return m_sqlDatabase;
 }
+
+bool SqlService::isTableExist(QString table)
+{
+    return m_sqlDatabase.tables().contains(table);
+}
+
+QMap<QString, QString> SqlService::getTableInfo(QString table)
+{
+    QMap<QString, QString> tableMap;
+
+    QString str = "PRAGMA table_info(" + table + ")";
+    m_sqlQuery.prepare(str);
+
+    if (m_sqlQuery.exec())
+    {
+        while (m_sqlQuery.next())
+        {
+            /* value(0) = 字段名; value(1) = 字段类型 */
+            tableMap[m_sqlQuery.value(1).toString()] = m_sqlQuery.value(2).toString();
+        }
+        return tableMap;
+    }
+    else
+    {
+        m_lastError = m_sqlQuery.lastError().text();
+        return tableMap;
+    }
+}
+
--- SqlService.h
+++ SqlService.h
@@ -2,6 +2,7 @@
 #define SQLSERVICE_H
 
 #include <QSqlQuery>
+#include <QVariantMap>
 
 class SqlService
 {
@@ -10,14 +11,26 @@ public:
 
     /* 打开sql文件，没有sql文件则创建一个对应数据库文件 */
     bool open(QString name);
+    /* 创建一个table, 传入一个key, value，对应顺序的变量名字（key），类型（value） */
+    bool createTable(QString table, QMap<QString, QString> map);
+    bool insertTable(QString table, QMap<QString, QVariant> map);
 
+    QString lastError();
 
     QSqlQuery& getSqlQuery();
     QSqlDatabase& getSqlDatabase();
+    bool isTableExist(QString table);
+
+
+private:
+    QMap<QString, QString> getTableInfo(QString table);
 
 private:
     QSqlQuery m_sqlQuery;
     QSqlDatabase m_sqlDatabase;
+    QString m_lastError;
+    /* key -> table; value: map */
+//    QMap<QString, QMap<QString, QString>> m_tableMap;
 };
 
 #endif // SQLSERVICE_H
